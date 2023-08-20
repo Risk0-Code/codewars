a35a966422c917267b5aaa98f22b1a347a142be3
Vincent Bouton <vincent.bouton.dev@gmail.com>
2023-08-20 16:22:09 +0200

    added link and kata description

--- js/isograms.js
+++ js/isograms.js
@@ -16,3 +16,11 @@ const word4 = "Banana";
 
 console.log(isIsogram(word3));
 console.log(isIsogram(word4));
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+// https://www.codewars.com/kata/54ba84be607a92aa900000f1
